ebf284d06d8cee583cc5cd17128a5412dc98048c
Wilkson Flor <wilksonflor12@gmail.com>
2023-03-25 11:16:30 -0300

    JS

---
+++ script.js
@@ -0,0 +1,9 @@
+function menuScroll(){
+    const scroll = document.querySelector('.header')
+    console.log('OI')
+}
+
+const menuOpen = document.querySelector('#btnMobileOpen').addEventListener('click', () =>{
+    const menuList = document.querySelector('#navMenuMobile')
+    
+})
