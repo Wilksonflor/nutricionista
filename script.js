9e011433e641192aea46f8837254b0ac05240197
Wilkson Flor <wilksonflor12@gmail.com>
2023-03-25 12:15:56 -0300

    Ajeitando o menu e começando a parte do slide

--- script.js
+++ script.js
@@ -1,9 +1,16 @@
-function menuScroll(){
-    const scroll = document.querySelector('.header')
-    console.log('OI')
-}
+// function menuScroll(){
+//     const scroll = document.querySelector('.header')
+//     console.log('OI')
+// }
 
 const menuOpen = document.querySelector('#btnMobileOpen').addEventListener('click', () =>{
-    const menuList = document.querySelector('#navMenuMobile')
+    const menuList = document.querySelector('.navMenuMobile')
+    const btnClose = document.querySelector('#btnMobileClose');
     
+    menuList.classList.toggle('open')
+    btnClose.addEventListener('click', () =>{
+        console.log('oi')
+       
+    })
+   
 })
